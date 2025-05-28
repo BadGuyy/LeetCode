7b8fc4e693bf7885dd416c9d5000d627c226a7c6
王彦泽 <1026684630@qq.com>
2025-05-28 19:53:37 +0800

    优化了滑动窗口算法，减少了巨量无用指针的位移

--- main.cpp
+++ main.cpp
@@ -3,31 +3,26 @@
 
 using namespace std;
 
-class Solution
+vector<int> twoSum(vector<int> &nums, int target)
 {
-public:
-    vector<int> twoSum(vector<int> &nums, int target)
+    for(int i = 0; i < nums.size(); ++i)
     {
-        for(int i = 0; i < nums.size(); ++i)
+        for(int j = i + 1; j < nums.size(); ++j)
         {
-            for(int j = i + 1; j < nums.size(); ++j)
-            {
-                if(nums[i] + nums[j] == target)
-                {   
-                    return {i, j};
-                }
+            if(nums[i] + nums[j] == target)
+            {   
+                return {i, j};
             }
         }
-        return nums;
     }
-};
+    return nums;
+}
 
 int main()
 {
-    Solution cls;
     vector<int> nums = {2, 7, 11, 15};
     int target = 9;
-    vector<int> result = cls.twoSum(nums, target);
+    vector<int> result = twoSum(nums, target);
     cout << result[0] << " " << result[1] << endl;
 
     system("pause");
--- medium/3.无重复字符的最长子串.cpp
+++ medium/3.无重复字符的最长子串.cpp
@@ -12,28 +12,63 @@ public:
     {
         // 滑动窗口法
         // 哈希集合，记录每个字符是否出现过
-        unordered_set<char> container;
-        int n = s.size();
-        // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
-        int rk = 0, ans = 0;
-        // 枚举左指针的位置，初始值隐性地表示为 -1
-        for(int i = 0; i < n; ++i)
-        {
-            if(i != 0)
-            {
-                // 左指针向右移动一格，移除一个字符
-                container.erase(s[i - 1]);
-            }
+        // unordered_set<char> container;
+        // int n = s.size();
+        // // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+        // int rk = 0, ans = 0;
+        // // 枚举左指针的位置
+        // for(int i = 0; i < n; ++i)
+        // {
+        //     if(i != 0)
+        //     {
+        //         // 左指针向右移动一格，移除一个字符
+        //         container.erase(s[i - 1]);
+        //     }
             
-            while(rk < n && container.count(s[rk]) == 0)
-            {
-                // 不断地移动右指针
-                container.insert(s[rk]);
-                ++rk;
-            }
-            // 第 i 到 rp 个字符是一个极长的无重复字符子串
-            ans = max(ans, rk - i);
-        }
+        //     while(rk < n && container.count(s[rk]) == 0)
+        //     {
+        //         // 不断地移动右指针
+        //         container.insert(s[rk]);
+        //         ++rk;
+        //     }
+        //     // 第 i 到 rp 个字符是一个极长的无重复字符子串
+        //     ans = max(ans, rk - i);
+        // }
+        // return ans;
+
+        // 滑动窗口法优化，不让左指针遍历到字符串末尾
+        // unordered_set<char> container;
+        // int ans = 0, left = 0, right = 0, tempLen = 0;
+        // while(right < s.size())
+        // {
+        //     char c = s[right];
+        //     if(container.count(c) == 0)
+        //     {
+        //         container.insert(c);
+        //         ++right;
+        //         ++tempLen;
+        //         ans = max(ans, tempLen);
+        //     }
+        //     else
+        //     {
+        //         container.erase(s[left]);
+        //         ++left;
+        //         --tempLen;
+        //     }
+        // }
+        // return ans;
+
+        // 滑动窗口法最终优化，不让左指针遍历到字符串末尾，使用数组记录上一次出现的位置
+        vector<int> last_index(128, -1);
+        int ans = 0;
+        // 左闭右开窗口左边界的位置
+        int left = -1;
+        for (int right = 0; right < s.size(); ++right) {
+            char c = s[right];
+            left = max(left, last_index[c]);
+            ans = max(ans, right - left);
+            last_index[c] = right;
+        } 
         return ans;
     }
 };
