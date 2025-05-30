75021785fceb22ffb8a85741a7181dc6abb38443
王彦泽 <1026684630@qq.com>
2025-05-30 22:06:34 +0800

    实现了中心扩展法，把空间复杂度压缩到了O(1)，官方题解中还有Manacher 算法，在feature/4-Manacher-algorithm分支上实现

--- main.cpp
+++ main.cpp
@@ -3,55 +3,111 @@
 
 using namespace std;
 
-double getKth(const vector<int>& nums1, const vector<int> nums2, int k)
-{
-    int len1 = nums1.size(), len2 = nums2.size();
-    // 两个数组根据二分法去掉左边被裁剪部分后的左边界
-    int left1 = 0, left2 = 0;
-
-    while (true)
+class Solution {
+public:
+    string longestPalindrome(string s)
     {
-        //处理边界条件
-        if (left1 == len1)
-            return nums2[left2 + k - 1];
-        if (left2 == len2)
-            return nums1[left1 + k - 1];
-        if (k == 1)
-            return min(nums1[left1], nums2[left2]);
+        // 中心扩展法
+        // 时间复杂度O(n^2)，空间复杂度O(1)
+        int start = 0, end = 0;
+        for (int i = 0; i < s.size(); i++)
+        {
+            auto [left1, right1] = expandAroundCenter(s, i, i);
+            auto [left2, right2] = expandAroundCenter(s, i, i + 1);
+            if (right1 - left1 > end - start)
+            {
+                start = left1;
+                end = right1;
+            }
+            if (right2 - left2 > end - start)
+            {
+                start = left2;
+                end = right2;
+            }
+        }
+        return s.substr(start, end - start + 1);
 
-        //常规处理
-        // 记录两个数组每次用二分法划分后比对的元素下标
-        int idx1 = min(left1 + k / 2 - 1, len1 - 1);
-        int idx2 = min(left2 + k / 2 - 1, len2 - 1);
-        if (nums1[idx1] <= nums2[idx2])
-        {
-            k -= idx1 + 1 - left1;
-            left1 = idx1 + 1;
-        }
-        else
-        {
-            k -= idx2 + 1 - left2;
-            left2 = idx2 + 1;
-        }
+        // // 动态规划
+        // // 时间复杂度O(n^2)，空间复杂度O(n^2)
+        // int len = s.size();
+        // if (len < 2) return s;
+        // // 定义dp数组，dp[i][j]表示s[i...j]是否为回文串
+        // // 这里双数组的上三角应该都为false，真正有意义的是下三角
+        // // 因为只需要判断i<=j的子串是否为回文串，下面的大循环已经排除了i>j的子串
+        // vector<vector<bool>> dp(len, vector<bool>(len, false));
+        // // 记录最长回文子串的起始位置和长度
+        // int begin = 0, maxLen = 1;
+        // // 初始化边界条件，长度为1的子串都是回文串
+        // for(int i = 0; i < len; i++) dp[i][i] = true;
+        // // 枚举子串长度
+        // for(int subLen = 2; subLen <= len; subLen++)
+        // {
+        //     // 枚举左边界
+        //     for(int left = 0; left <= len - subLen; left++)
+        //     {
+        //         // 枚举右边界
+        //         int right = left + subLen - 1;
+        //         if(s[left] != s[right])
+        //             dp[left][right] = false;
+        //         else if(subLen <= 3)
+        //             // 左右边界相同时，长度小于等于3的子串都是回文串
+        //             dp[left][right] = true;
+        //         else
+        //             dp[left][right] = dp[left + 1][right - 1];
+        //         // 更新最长回文子串
+        //         if(dp[left][right] == true && subLen > maxLen)
+        //         {
+        //             begin = left;
+        //             maxLen = subLen;
+        //         }
+        //     }
+        // }
+        // return s.substr(begin, maxLen);
+
+        
+        // // 暴力法，枚举所有子串，判断是否为回文串
+        // // 时间复杂度O(n^3)，空间复杂度O(1)
+        // // 不可行，超时
+        // string res;
+        // for(int len = s.size(); len > 0; --len)
+        // {
+        //     for(int i = 0; i + len <= s.size(); ++i)
+        // {
+        //     res = s.substr(i, len);
+        //     if(isSymmetric(res))
+        //         return res;
+        // }
+        // return res;
     }
-}
-double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2)
-{
-    // 通过利用二分法查找第k小的元素来求解中位数
-        int len = nums1.size() + nums2.size();
-        if (len % 2 != 0)
-            return getKth(nums1, nums2, (len + 1) / 2);
-        else
-            return (getKth(nums1, nums2, len / 2) +
-                getKth(nums1, nums2, len / 2 + 1)) / 2.0;
-}
+
+    // 判断字符串是否为回文串
+    bool isSymmetric(string &s)
+    {
+        for(int i = 0; i <= s.size() / 2; ++i)
+            if(s[i] != s[s.size() - i - 1])
+                return false;
+        return true;
+    }
+    
+    // 对字符串的指定子串进行中心扩展
+    pair<int, int> expandAroundCenter(string &s, int left, int right)
+    {
+        while(left >= 0 && right < s.size() && s[left] == s[right])
+        {
+            left--;
+            right++;
+        }
+        return {left + 1, right - 1};
+    }
+};
 
 
 int main()
 {
-    vector<int> nums1 = {1,2,3,4,5}, nums2 = {6,7,8,9,10,11,12,13,14,15,16,17};
-    double result = findMedianSortedArrays(nums1, nums2);
-    cout << result << endl;
+    Solution s;
+    string str = "babad";
+    string res = s.longestPalindrome(str);
+    cout << res << endl;
 
     system("pause");
     return 0;
--- medium/5.最长回文子串.cpp
+++ medium/5.最长回文子串.cpp
@@ -9,44 +9,97 @@ class Solution {
 public:
     string longestPalindrome(string s)
     {
-        // 动态规划
-        // 时间复杂度O(n^2)，空间复杂度O(n^2)
-        int len = s.size();
-        if (len < 2) return s;
-        // 定义dp数组，dp[i][j]表示s[i...j]是否为回文串
-        // 这里双数组的上三角应该都为false，真正有意义的是下三角
-        // 因为只需要判断i<=j的子串是否为回文串，下面的大循环已经排除了i>j的子串
-        vector<vector<bool>> dp(len, vector<bool>(len, false));
-        // 记录最长回文子串的起始位置和长度
-        int begin = 0, maxLen = 1;
-        // 初始化边界条件，长度为1的子串都是回文串
-        for(int i = 0; i < len; i++) dp[i][i] = true;
-        // 枚举子串长度
-        for(int subLen = 2; subLen <= len; subLen++)
+        // 中心扩展法
+        // 时间复杂度O(n^2)，空间复杂度O(1)
+        int start = 0, end = 0;
+        for (int i = 0; i < s.size(); i++)
         {
-            // 枚举左边界
-            for(int left = 0; left <= len - subLen; left++)
+            auto [left1, right1] = expandAroundCenter(s, i, i);
+            auto [left2, right2] = expandAroundCenter(s, i, i + 1);
+            if (right1 - left1 > end - start)
             {
-                // 枚举右边界
-                int right = left + subLen - 1;
-                if(s[left] != s[right])
-                    dp[left][right] = false;
-                else if(subLen <= 3)
-                    // 左右边界相同时，长度小于等于3的子串都是回文串
-                    dp[left][right] = true;
-                else
-                    dp[left][right] = dp[left + 1][right - 1];
-
-                // 更新最长回文子串
-                if(dp[left][right] == true && subLen > maxLen)
-                {
-                    begin = left;
-                    maxLen = subLen;
-                }
+                start = left1;
+                end = right1;
+            }
+            if (right2 - left2 > end - start)
+            {
+                start = left2;
+                end = right2;
             }
         }
+        return s.substr(start, end - start + 1);
 
-        return s.substr(begin, maxLen);
+        // // 动态规划
+        // // 时间复杂度O(n^2)，空间复杂度O(n^2)
+        // int len = s.size();
+        // if (len < 2) return s;
+        // // 定义dp数组，dp[i][j]表示s[i...j]是否为回文串
+        // // 这里双数组的上三角应该都为false，真正有意义的是下三角
+        // // 因为只需要判断i<=j的子串是否为回文串，下面的大循环已经排除了i>j的子串
+        // vector<vector<bool>> dp(len, vector<bool>(len, false));
+        // // 记录最长回文子串的起始位置和长度
+        // int begin = 0, maxLen = 1;
+        // // 初始化边界条件，长度为1的子串都是回文串
+        // for(int i = 0; i < len; i++) dp[i][i] = true;
+        // // 枚举子串长度
+        // for(int subLen = 2; subLen <= len; subLen++)
+        // {
+        //     // 枚举左边界
+        //     for(int left = 0; left <= len - subLen; left++)
+        //     {
+        //         // 枚举右边界
+        //         int right = left + subLen - 1;
+        //         if(s[left] != s[right])
+        //             dp[left][right] = false;
+        //         else if(subLen <= 3)
+        //             // 左右边界相同时，长度小于等于3的子串都是回文串
+        //             dp[left][right] = true;
+        //         else
+        //             dp[left][right] = dp[left + 1][right - 1];
+        //         // 更新最长回文子串
+        //         if(dp[left][right] == true && subLen > maxLen)
+        //         {
+        //             begin = left;
+        //             maxLen = subLen;
+        //         }
+        //     }
+        // }
+        // return s.substr(begin, maxLen);
+
+        
+        // // 暴力法，枚举所有子串，判断是否为回文串
+        // // 时间复杂度O(n^3)，空间复杂度O(1)
+        // // 不可行，超时
+        // string res;
+        // for(int len = s.size(); len > 0; --len)
+        // {
+        //     for(int i = 0; i + len <= s.size(); ++i)
+        // {
+        //     res = s.substr(i, len);
+        //     if(isSymmetric(res))
+        //         return res;
+        // }
+        // return res;
+    }
+
+    // 判断字符串是否为回文串
+    bool isSymmetric(string &s)
+    {
+        for(int i = 0; i <= s.size() / 2; ++i)
+            if(s[i] != s[s.size() - i - 1])
+                return false;
+        return true;
+    }
+    
+    // 对字符串的指定子串进行中心扩展
+    pair<int, int> expandAroundCenter(string &s, int left, int right)
+    {
+        while(left >= 0 && right < s.size() && s[left] == s[right])
+        {
+            left--;
+            right++;
+        }
+        return {left + 1, right - 1};
     }
 };
 // @lc code=end
